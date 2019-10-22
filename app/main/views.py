23f486509c8ecac1f8d718b2b43ae84ce11dc594
abner <axocop@ufm.edu>
2019-10-21 22:03:58 -0600

    Configuring the reviews

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
+from ..models import Movie
+#Review = review.Review
