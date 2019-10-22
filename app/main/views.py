5c50b467437ce8c63e78451f947e5a26e955c1b3
abner <axocop@ufm.edu>
2019-10-21 22:05:13 -0600

    View root page function that returns the index page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,17 @@ from .forms import ReviewForm
 from ..models import Review
 from ..models import Movie
 #Review = review.Review
+
+@main.route('/')
+def index():
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('.search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
