1e966fef4c25abce136adeaf8f4bbd589fb83abf
abner <axocop@ufm.edu>
2019-10-21 21:18:48 -0600

    Showing reviews on the movie

--- app/models.py
+++ app/models.py
@@ -20,3 +20,15 @@ class Review:
 
     def save_review(self):
         Review.all_reviews.append(self)
+
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls, id):
+        response = []
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+        return response
