124301b75010752a6384a4f6fc07742699e5ea79
abner <axocop@ufm.edu>
2019-10-21 22:59:56 -0600

    Updating the package requirements

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from flask_script import Manager,Server
 from app import create_app,db
 from app.models import User,Role
 
-#creating app
 app = create_app("production")
 
 manager = Manager(app)
@@ -11,9 +10,6 @@ manager.add_command("server",Server)
 
 @manager.command
 def test():
-    """
-
-    """
     import unittest
     tests = unittest.TestLoader().discover("test")
     unittest.TextTestRunner(verbosity=2).run(tests)
@@ -21,7 +17,6 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
-
 
 if __name__ == "__main__":
     manager.run()
