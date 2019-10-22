b2699eb5a0bb228b5e8c7f85af7c3a26a796b5a2
abner <axocop@ufm.edu>
2019-10-21 21:11:52 -0600

    Creating my function

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app():
+    pass
