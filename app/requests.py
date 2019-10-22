9dc228f3bfafb7811c1268904c50bceabecb5760
abner <axocop@ufm.edu>
2019-10-21 21:57:00 -0600

    Configuring the function that gets the json

--- app/requests.py
+++ app/requests.py
@@ -14,3 +14,17 @@ def configure_request(app):
     global api_key,base_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+        movie_results = None
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+    return movie_results
