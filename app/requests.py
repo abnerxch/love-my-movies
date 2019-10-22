0689a85c2cfb7f88c994c4505eb5cc78aec5fce8
abner <axocop@ufm.edu>
2019-10-21 21:55:37 -0600

    Configuring request, pending: get movies

--- app/requests.py
+++ app/requests.py
@@ -7,3 +7,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 #Movie = movie.Movie
 # Getting api key
 api_key = None
+
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
