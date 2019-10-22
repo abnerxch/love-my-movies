cf2a81029e782cd0323170afe2c11c74bdded583
abner <axocop@ufm.edu>
2019-10-21 22:56:17 -0600

    Creating a Manager to my app

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User,Role
+
+#creating app
+app = create_app("production")
+
+manager = Manager(app)
+manager.add_command("server",Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+if __name__ == "__main__":
+    manager.run()
